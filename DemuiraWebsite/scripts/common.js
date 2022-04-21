285444422d3787f26531654f608f054680f7881d
Joe <jaaronclarke@gmail.com>
2022-04-21 16:56:59 -0600

    contact form, linked page updates

--- DemuiraWebsite/scripts/common.js
+++ DemuiraWebsite/scripts/common.js
@@ -1,50 +1,13 @@
-function Common() {
-    let self = this;
-    /* Properties */
-    this.promoBar =
-        {
-            promoItems: null,
-            currentItem: 0,
-            numberOfItems: 0,
-        };
-    /* Methods */
-    this.initializePromo = function () {
-        /* Get all items in promo bar */
-        let promoItems = $("#promo > div");
-        /* Set values */
-        this.promoBar.promoItems = promoItems;
-        this.promoBar.numberOfItems = promoItems.length;
-        /* Initiate promo loop to show next item */
-        this.startDelay();
-    }
-        this.startDelay = function () {
-            /* Wait 4 seconds then show the next message */
-            setTimeout(function () {
-                self.showNextPromoItem()
-            }, 4000);
-    } 
-    this.showNextPromoItem = function () {
-        /* Fade out the current item */
-        $(self.promoBar.promoItems).fadeOut("slow").promise().done(function () {
-            /* Increment current promo item counter */
-            if (self.promoBar.currentItem >= (self.promoBar.numberOfItems - 1)) {
-                /* Reset counter to zero */
-                self.promoBar.currentItem = 0;
-            } else {
-                /* Increase counter by 1 */
-                self.promoBar.currentItem++;
-            }
-            /* Fade in the next item */
-            $(self.promoBar.promoItems).eq(self.promoBar.currentItem).fadeIn("slow", function () {
-                /* Delay before showing next item */
-                self.startDelay();
-            });
-        });
-    }
-}
-$(document).ready(function () {
-    /* Instantiate new Common class */
-    app.common = new Common();
-    /* Initialize the Promo bar */
-    app.common.initializePromo();
-});
+var terms = [
+    "Dispatch with 24 Hours",
+    "Free Shipping",
+    "New Stainless Steel components",
+    "Buy 5 pieces and save 30%",
+  ];
+  
+  function rotateTerm() {
+    var ct = $("#rotate").data("term") || 0;
+    $("#rotate").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn(800)
+      .delay(4000).fadeOut(400, rotateTerm);
+  }
+  $(rotateTerm);
